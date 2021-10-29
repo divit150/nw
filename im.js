ee6cd0f4ef4ebe0d129bf1713b30e7c4ee9a61d4
divit150 <77146400+divit150@users.noreply.github.com>
2021-10-29 21:39:13 +0530

    Add files via upload

---
+++ im.js
@@ -0,0 +1,24 @@
+video="";
+
+function preload(){
+    video=createVideo('Desert Zombie Chase(2).mp4')
+video.hide();
+}
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+function draw(){
+    image(video,0,0,480,380);
+}
+function start(){
+    objectDeyeyor=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status : detecting objects";
+}
+ function modelLoaded(){
+     console.log("model loaded");
+     status=true;
+     video.loop();
+     video.speed(1);
+     video.volume(0);
+    }
